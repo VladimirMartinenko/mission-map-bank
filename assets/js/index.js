ccd2c0299114dc4d2d972de24975087debaef1ca
Vladimir Martinenko <teslabest87@gmail.com>
2022-04-23 23:18:01 +0300

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,24 @@
-client1 = {
+'use strict'
+
+const client1 = {
   fullName: "Ivanov Ivan Ivanovich",
   clientLevel: "basik",
   money: 100000,
   bankName: "Sib",
 };
-client2 = {
+const client2 = {
   fullName: "Petrov Petr Petrovich",
   clientLevel: "medium",
   money: 100000,
   bankName: "Ukr",
 };
-client3 = {
+const client3 = {
   fullName: "Antonov Anton Antonovich",
   clientLevel: "premium",
   money: 100000,
   bankName: "Allo",
 };
-bank1 = {
+const bank1 = {
   bankName: "Sib",
   clientLevels: {
     basik: {
@@ -30,7 +32,7 @@ bank1 = {
     },
   },
 };
-bank2 = {
+const bank2 = {
   bankName: "Ukr",
   clientLevels: {
     minimum: {
@@ -44,7 +46,7 @@ bank2 = {
     },
   },
 };
-bank3 = {
+const bank3 = {
   bankName: "Allo",
   clientLevels: {
     start: {
@@ -82,37 +84,60 @@ const {
 } = bank3;
 
 let map = new Map();
+map.set('basik',level1)
+map.set('pro',level2)
+map.set('platinum',level3)
 
-map.set(client1.clientLevel, level1);
 let map2 = new Map();
-map2.set(client2.clientLevel, rate2);
+map2.set('minimum', rate2);
+map2.set('medium', rate2);
+map2.set('maximum', rate2);
 let map3 = new Map();
-map3.set(client3.clientLevel, tariffPlan3);
+map3.set('start', tariffPlan3);
+map3.set('large', tariffPlan3);
+map3.set('premium', tariffPlan3);
 
 let bank = new Map();
-bank.set(client1.bankName, map);
-bank.set(client2.bankName, map2);
-bank.set(client3.bankName, map3);
+bank.set('Sib', map);
+bank.set('Ukr', map2);
+bank.set('Allo', map3);
 
 function priceDiscount(client, price) {
   const bankMap = bank.get(client.bankName);
-  if (bank.has(client.bankName) === false) {
-    throw new Error("поддельная карта");
-  } else if (bankMap.get(client.clientLevel) !== undefined && client.money > price
-  ) {
-    let priceDisc = price - (bankMap.get(client.clientLevel) / 100) * price;
-    client.money -= priceDisc;
-    return priceDisc;
-  } else if (
-    bankMap.get(client.clientLevel) === undefined && client.money > price) {
-    client.money -= price;
-    return price;
-  } else if (bankMap.get(client.clientLevel) === undefined) {
-    throw new RangeError(`не хватает денег до покупки ${price - client.money}`);
-  } else {throw new RangeError(
-      `не хватает денег до покупки с учетом скидки ${
-        price - (bankMap.get(client.clientLevel) / 100) * price - client.money
-      }`
-    );
+if (bankMap === undefined) {
+  throw new Error('поддельная карта');
+  } else {
+    const discount = bankMap.get(client.clientLevel);
+    const priceWithDiscount = 
+    discount === undefined ? price : price - (discount/100)*price;
+    if(priceWithDiscount > client.money) {
+      throw new RangeError(
+        `не хватает денег до покупки ${priceWithDiscount - client.money}`
+      );
+    }else {
+      client.money -= priceWithDiscount;
+      return priceWithDiscount;
+    }
   }
+
 }
+//   if (bank.has(client.bankName) === false) {
+//     throw new Error("поддельная карта");
+//   } else if (bankMap.get(client.clientLevel) !== undefined && client.money > price
+//   ) {
+//     let priceDisc = price - (bankMap.get(client.clientLevel) / 100) * price;
+//     client.money -= priceDisc;
+//     return priceDisc;
+//   } else if (
+//     bankMap.get(client.clientLevel) === undefined && client.money > price) {
+//     client.money -= price;
+//     return price;
+//   } else if (bankMap.get(client.clientLevel) === undefined) {
+//     throw new RangeError(`не хватает денег до покупки ${price - client.money}`);
+//   } else {throw new RangeError(
+//       `не хватает денег до покупки с учетом скидки ${
+//         price - (bankMap.get(client.clientLevel) / 100) * price - client.money
+//       }`
+//     );
+//   }
+// }
